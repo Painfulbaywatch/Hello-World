0842725455ee39016d0da0c60a114977b5da64af
Painfulbaywatch <andrewwillis@myop.ac.nz>
2022-07-24 16:23:10 +1200

    a second change to make sure this commit thing works now

--- My First Project/src/hellowworld.java
+++ My First Project/src/hellowworld.java
@@ -2,7 +2,7 @@
 public class hellowworld {
 
 	public static void main(String[] args) {
-		// aaand, sending message to user yet again
+		// showing message to user yet again
 		System.out.println("Hello world!");
 	}
 
